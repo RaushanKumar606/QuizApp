7a1937dbfff60ecc88a71436e0f3f4b864ec08ca
RAUSHAN KUMAR SINGH <roshankumarsingh964@gmail.com>
2025-12-03 19:24:47 +0530

    correction

--- client/src/utils/api.jsx
+++ client/src/utils/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({ 
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://quiz-app-zeta-five-56.vercel.app/api',
   headers: {
     'Content-Type': 'application/json',
   }
